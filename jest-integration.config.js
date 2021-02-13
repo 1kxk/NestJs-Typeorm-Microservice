7643344ac249ec484ff7a4939f93540061500415
Caio <caiow.wk@gmail.com>
2021-02-13 14:40:07 -0300

    style: remove semicolon

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('./jest.config')
 config.testMatch = ['**/*.test.ts']
 
-module.exports = config;
+module.exports = config
--- jest-unit.config.js
+++ jest-unit.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('./jest.config')
 config.testMatch = ['**/?(*.)+(spec|test).[tj]s?(x)']
 
-module.exports = config;
+module.exports = config
