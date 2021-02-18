6097b18c34238ec49c15993dee9cc4cbd734c74c
Caio Fernandes <caiow.wk@gmail.com>
2021-02-18 20:31:34 -0300

    Update users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common'
+import { MulterModule } from '@nestjs/platform-express'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AuthModule } from '../../shared/modules/auth/auth.module'
 import { RolesGuard } from './guards/roles.guard'
-import { UsersIsUser } from './guards/user-is-user.guard'
+import { UserIsUser } from './guards/user-is-user.guard'
 import { UsersController } from './users.controller'
 import { UsersRepository } from './users.repository'
 import { UsersService } from './users.service'
@@ -11,10 +12,13 @@ import { UsersService } from './users.service'
 @Module({
   imports: [
     TypeOrmModule.forFeature([UsersRepository]),
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    MulterModule.register({
+      dest: __dirname + '/../../../tmp'
+    })
   ],
   controllers: [UsersController],
-  providers: [UsersService, RolesGuard, UsersIsUser],
+  providers: [UsersService, RolesGuard, UserIsUser],
   exports: [UsersService]
 })
 export class UsersModule {}
